9719b9e8c289bfaf0cd1f8bae0366e66e1c5c6cc
Kenneth Rasmussen <kras3@hotmail.com>
2017-09-18 10:05:03 -0400

    Exercise 2

---
+++ bash/Lab2/Demo3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Exercise 2
+
+echo "hi
+there
+gorgeous"
+
+echo -n "helb wold" |
+sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl" |
+tr "h" "H" |
+tr "w" "W" |
+awk '(print $1 "\0x20" $2 "\41")'
