7ab3227efd6a17123c6bcafe8cfe447d65f0b27c
Kenneth Rasmussen <kras3@hotmail.com>
2017-09-18 08:59:39 -0400

    Demo 2

---
+++ bash/Lab2/Demo2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# demo script
+
+export MYNAME="Kenneth"
+mytitle="Supreme Commander"
+myhostname=hostname
+weekday=(date +%A)
+
+echo "Welcome to $myhostname, $mytitle $MYNAME! Today is $weekday"
