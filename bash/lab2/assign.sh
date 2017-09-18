0f9daf9f51d222f61092fe8a0c8f97638178bf9b
Kenneth Rasmussen <kras3@hotmail.com>
2017-09-18 14:18:44 -0400

    Assign ment for lab 2

--- bash/lab2/assign.sh
+++ bash/lab2/assign.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+netint=$(ifconfig | sed -n '1p' | awk '{print $1;}') #network interface on line 1, 1st word
+myip=$(ip address | grep "$netint" | sed -n '3p' | awk '{print $2;}') #use netint to lookup ip address
+speed=$(ethtool $netint | awk '/Speed/' | awk '{print $2;}') #use ethtool to lookup speed on netint
+
+echo "current information:
+$netint
+$myip
+$speed"
