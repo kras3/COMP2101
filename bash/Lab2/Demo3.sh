4eaec860fb0b592e79ecd07c279334cc7b1b84a5
Kenneth Rasmussen <kras3@hotmail.com>
2017-09-18 10:08:16 -0400

    edit

--- bash/Lab2/Demo3.sh
+++ bash/Lab2/Demo3.sh
@@ -7,7 +7,7 @@ there
 gorgeous"
 
 echo -n "helb wold" |
-sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl" |
+sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" |
 tr "h" "H" |
 tr "w" "W" |
 awk '(print $1 "\0x20" $2 "\41")'
