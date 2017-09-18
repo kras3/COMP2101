ee26438c5dd900850055bff4f9641f4b6e5d5632
Kenneth Rasmussen <kras3@hotmail.com>
2017-09-18 12:13:31 -0400

    Demo4 Math adjustment

--- bash/Lab2/Demo4.sh
+++ bash/Lab2/Demo4.sh
@@ -1,30 +1,11 @@
 #!/bin/bash
 
-# Create an array named colours and give it the following data values: red, green, blue
-# Create a hash named animals and give it the following data elements: key red - value cardinal,
-#key green - value frog, key blue - value lobster
-# Use echo to display all the elements of the colours array, both by using the index values, and by
-#using @ as an index
-# Do the same for the same for the animals array
-# Create a variable named num and give it a value of 0
-# Use echo to display the data from the colours array corresponding to the value of $num
-#followed by the value from the animals hash which uses the colour from the colours array as a
-#key to find an animal name e.g. red cardinal, green frog, blue lobster
-# Create a script to define the array and hash as above, and ask the user for a number for the num
-#variable to select a combination to print out
+read -p "Give me two numbers: " firstnum secondnum
 
-colours=("red" "green" "blue")
-
-declare -A animals
-animals=([red]="cardinal" [green]="frog" [blue]="lobster")
-
-echo "My colours are: ${colours[*]}"
-echo "the animals array contains: ${animals[red]}, ${animals[green]}, ${animals[blue]}."
-
-
-read -p "What number do you want 0 to 2: " num
-
-
-mycolour=${colours[$num]}
-myanimal=${animals[$mycolour]}
-echo "Number $num refers to a $mycolour $myanimal"
+echo "$firstnum plus $secondnum is $(($firstnum + $secondnum))"
+echo "$firstnum minus $secondnum is $(($firstnum - $secondnum))"
+echo "$firstnum times $secondnum is $(($firstnum * $secondnum))"
+echo "$firstnum divided by $secondnum is $(($firstnum / $secondnum))"
+echo It is also $(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+echo "The remainder when dividing $firstnum by $secondnum is $(($firstnum % $secondnum))"
+echo "$firstnum to the power of $secondnum is $(bc <<< "$firstnum ^ $secondnum")"
